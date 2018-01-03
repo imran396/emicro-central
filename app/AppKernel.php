45727d8963004b04d7cfdc61bb5f42726c807bf2
imran396 <imran3968@gmail.com>
2018-01-03 22:09:24 +0600

    travis file added.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Emicro\Bundle\CoreBundle\EmicroCoreBundle(),
+            new Emicro\Bundle\TestBundle\EmicroTestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
